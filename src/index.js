fa52aa5404674eb632f604f97b5dbcf2d3be3af6
MauroAC <mauroac26@gmail.com>
2021-09-25 10:47:28 -0300

    Creación y conexión de base de datos

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const exp = require('constants');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path')
+const app = express();
+
+const { mongoose } = require('./database');
+
+//setting
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json())
+
+//Routes
+app.use('/api/presupuesto', require('./routes/presupuesto.routes'))
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+})
