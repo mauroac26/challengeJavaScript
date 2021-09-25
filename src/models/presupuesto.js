39f2f8677e41945f560aa15c7bb8fe45a18e8958
MauroAC <mauroac26@gmail.com>
2021-09-25 14:40:33 -0300

    Definicion de operaciones

--- src/models/presupuesto.js
+++ src/models/presupuesto.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const { Schema} = mongoose;
+const { Schema } = mongoose;
+var Float = require('mongoose-float').loadType(mongoose, 4);
 
 const presupuestoShema = new Schema({
     concepto: { type: String, require: true },
-    monto: { type: Float32Array, require: true },
+    monto: { type: Float, require: true },
     fecha: { type: Date, require: true },
     tipo: { type: String, require: true }
 });
 
 module.exports = mongoose.model('Presupuesto', presupuestoShema);
-
-
--- src/routes/presupuesto.routes.js
+++ src/routes/presupuesto.routes.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        status: 'API Works!'
-    });
+const Presupuesto = require('../models/presupuesto')
+
+router.get('/', async (req, res) => {
+    const presupuestos = await Presupuesto.find();
+    res.json(presupuestos);
+});
+
+router.get('/:id', async (req, res) => {
+    const presupuesto = await Presupuesto.findById(req.params.id);
+    res.json(presupuesto);
+});
+
+router.post('/', async (req, res) => {
+    const { concepto, monto, fecha, tipo } = req.body;
+    const presupuesto = new Presupuesto({concepto, monto, fecha, tipo});
+    await presupuesto.save();
+    res.json({status: 'Presupuesto guardado'});
+});
+
+router.put('/:id', async (req, res) => {
+    const { concepto, monto, fecha, tipo } = req.body;
+    const newPresupuesto = { concepto, monto, fecha, tipo };
+    await Presupuesto.findByIdAndUpdate(req.params.id, newPresupuesto)
+    res.json({status: "Presupuesto actualizado"});
+});
+
+router.delete('/:id', async (req, res) => {
+    await Presupuesto.findByIdAndRemove(req.params.id);
+    res.json({status: 'Presupuesto Eliminado'});
 });
 
 module.exports = router;
